476dcb6c2470f22822b6fcac326da6378ef93d03
saad942 <ammarisaad343@gmail.com>
2024-04-03 20:37:07 +0000

    Create sign in API route

--- api/index.js
+++ api/index.js
@@ -7,25 +7,20 @@ import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose
-    .connect(process.env.MONGO)
-    .then(() => {
-        console.log('Connected to MongoDB!');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
-
-
-
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 }
 );
 
@@ -33,14 +28,15 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
+
 
 
 
